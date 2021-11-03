d3af3b1ab48da5b6166c694bc586bbaefac8a168
KAVARA-TechX <kavara.tech@gmail.com>
2021-11-03 20:23:35 +0530

    set code

--- app/main.py
+++ app/main.py
@@ -8,15 +8,5 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    s = smtplib.SMTP('smtp.gmail.com','587')
-    s.ehlo()
-    s.starttls()
-    s.ehlo()
-    s.login("kavara.tracker@gmail.com", "Kavara@123")
-    SUBJECT="Sending mail"
-    TEXT="Hello"
-    message = 'Subject: {}\n\n{}'.format(SUBJECT, TEXT)
-    s.sendmail("kavara.tracker@gmail.com", "ka340689@gmail.com",message )
-    s.quit()
     return "Sent"
 
